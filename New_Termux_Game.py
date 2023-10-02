30aeca2cfcf165974bff6d867e0b9eb4e1215516
Marven-HackersBl4khat <146468163+Marven-HackersBl4khat@users.noreply.github.com>
2023-10-02 21:50:21 +0100

    Add files via upload

---
+++ New_Termux_Game.py
@@ -0,0 +1,170 @@
+import sys
+import time
+topic = "*Alphabet world game*"
+topic_A = topic.upper()
+print (topic_A)
+
+import time
+time.sleep(1)
+for i in range(1,5):
+	time.sleep(2)
+	print("|∆________∆-∆_________________________________∆____∆|")
+	print("____________________________________________________")
+time.sleep(2)
+for i in range(1,2):
+	print("_____________________")
+	print("Author:  Marvel")
+	print("______________________")
+	time.sleep(2)
+	print("Team: Marven_Hackers")
+	print("______________________")
+	time.sleep(2)
+	print("Tool: Termux_Game")
+	print("______________________")
+	time.sleep(2)
+for i in range(1,5):
+	time.sleep(2)
+	print("|∆________∆-∆_________________________________∆____∆|")
+	print("____________________________________________________")
+print()
+print()
+print()
+
+print()
+user_n = input("insert your name: ")
+print(f"{user_n} do you really wanna play this game?")
+user_choice = input(": ")
+if user_choice == "yes": 
+ print()
+ import time
+import sys
+for I in range (1,5):
+ time.sleep(1)
+ print ("loading_game...5%")
+for I in range (5,15):
+ time.sleep(0.4)
+ print ("loading_game...15%")
+for I in range (15,22):
+ time.sleep(0.3)
+ print ("loading_game...30%")
+for I in range (22,72):
+ time.sleep(0.2)
+ print ("loading_game...70%")
+for I in range (72,89):
+ time.sleep(0.1)
+ print ("loading_game...90%")
+for I in range (89,100):
+ print ("loading_game...99%")
+print ("loading_game...100%")
+print()
+user_n = input ("insert your real name to start game now!\n: ")
+import time
+while True:
+ user_choice =input ("Enter Any Alphabet From A-Z (enter 'ex' to exit)\n: ")
+ print ()
+ if user_choice == "ex":
+  time.sleep(0.4)
+  for I in range (1,7):
+   time.sleep(1.1)
+   print (f"{user_n} trying to exit game... failed!")
+  print("exited game... successfully!")
+  break
+ if user_choice == "a":
+  time.sleep(1.3)
+  print (f"loading results for '{user_choice}'")
+  time.sleep(1.2)
+  print ("A is for 'apple'")
+  time.sleep(1.1)
+  print ()
+ elif user_choice == "b":
+  
+  print ("loading results for 'b'...")
+  time.sleep(1.2)
+  print ("B is for 'ball'")
+  time.sleep(1.2)
+  print ()
+ elif user_choice == "c":
+  print (f"loading results for '{user_choice}'")
+  time.sleep(1.2)
+  print ("C is for 'car'")
+  print ()
+ elif user_choice == "d":
+  for I in range (1,5):
+   time.sleep(1.2)
+   print (f"loading results for '{user_choice}'...")
+  print ("D is for dog\n")
+  time.sleep(1.4)
+ elif user_choice == "":
+  print (f"{user_n},you are very foolish") 
+  print ()
+ elif user_choice == "e":
+  print ("loading results for 'E'")
+  time.sleep(1)
+  print ("E is for 'egg'\n")
+  time.sleep(1)
+ elif user_choice == "f":
+  print ("F is for 'fork'\n")
+ elif user_choice == "g":
+  print ("G is for 'gun'\n")
+ elif user_choice == "h":
+  print ("H is for 'hug'\n")
+  time.sleep(1)
+ elif user_choice == "i":
+  time.sleep(0.5)
+  print ("I is for 'ice'\n")
+  time.sleep(0.8)
+ elif user_choice == "j":
+  print ("J is for 'jock'\n")
+ elif user_choice == "k":
+  time.sleep(0.6)
+  print (f"{user_choice} is for 'kid'\n")
+ elif user_choice == "l":
+  print (f"{user_choice} is for 'land'\n")
+ elif user_choice == "m":
+  print (f"{user_choice} is for 'mango'\n")
+ elif user_choice == "n":
+  print (f"{user_choice} is for 'nurse'\n")
+ elif user_choice == "o":
+  for I in range (3,10):
+   print (f"loading results for '{user_choice},...")
+   time.sleep(0.8)
+  print (f"{user_choice} is for 'okra' and 'onion\n'")
+ elif user_choice == "p":
+  print (f"{user_choice} is for 'plantain'\n")
+ elif user_choice == "q":
+  print (" Q is for 'queen'\n")
+ elif user_choice == "r":
+  print ("R is for 'rice'\n")
+ elif user_choice == "s":
+  for I in range (1,6):
+   print (f"loading results for '{user_choice}'...")
+   time.sleep(0.8)
+  print ("S is for 'song' and 'sand'\n")
+ elif user_choice == "t":
+  print (f"{user_choice} is for 'telephone'\n")
+ elif user_choice == "u":
+  print ("U is for 'umbrella'\n")
+ elif user_choice == "v":
+  for I in range (2,6):
+   print (f"loading results for '{user_choice}'...")
+   time.sleep(1)
+  print ("V is for 'van'\n")
+ elif user_choice == "w":
+  print (f"{user_choice} is for 'wagon'\n")
+ elif user_choice == "x":
+  print (f"{user_choice} is for 'your ex'\n")
+ elif user_choice == "y":
+  print (f"{user_choice} is for 'yam'\n")
+ elif user_choice == "z":
+  for I in range (5,9):
+   print (f"loading_results_for '{user_choice}'")
+   time.sleep(0.9)
+  print (f"{user_choice} is for 'Zoo'\n")
+ else:
+  print (f" '{user_choice}' is not defined")
+else:
+   print()
+sys.exit("bye...\nYou would have said 'yes' ")
+sys.exit("please rate our game...")
+
+
